c59dfb25b359e34ae62ca451f39175975616db94
Hans Desmet <hans.desmet@vdab.be>
2020-03-20 14:23:30 +0100

    jacoco

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 class GoedeDoelTest {
 	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
@@ -30,6 +32,10 @@ class GoedeDoelTest {
 	@Test
 	void doelenMetVerschillendeNaamZijnVerschillend() {
 		assertThat(doel).isNotEqualTo(new GoedeDoel("WWF"));
+	}
+	@Test
+	void doelVerschiltVanEenObjectMetEenAnderType() {
+		assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
 	}
 
 
